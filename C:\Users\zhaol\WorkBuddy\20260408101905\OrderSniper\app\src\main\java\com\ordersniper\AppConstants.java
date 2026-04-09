059a7019684f7aea11f9e9b22b03a2363679295e
zhaolianduo <36400601+zhaolianduo@users.noreply.github.com>
2026-04-09 18:47:52 +0800

    Remove wrongly pathed file C:\Users\zhaol\WorkBuddy\20260408101905\OrderSniper\app\src\main\java\com\ordersniper\AppConstants.java

--- C:\Users\zhaol\WorkBuddy\20260408101905\OrderSniper\app\src\main\java\com\ordersniper\AppConstants.java
+++
@@ -1,7 +0,0 @@
-package com.ordersniper;
-
-public class AppConstants {
-    public static final String PREFS_NAME = "order_sniper_prefs";
-    public static final String KEY_KEYWORDS = "keywords";
-    public static final String KEY_FLOAT_WINDOW_SHOWN = "float_window_shown";
-}
