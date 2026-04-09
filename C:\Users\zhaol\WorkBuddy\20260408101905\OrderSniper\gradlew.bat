34b09d0889748c01a64c6c0f5daaf6337b6ec925
zhaolianduo <36400601+zhaolianduo@users.noreply.github.com>
2026-04-09 18:46:22 +0800

    Add C:\Users\zhaol\WorkBuddy\20260408101905\OrderSniper\gradlew.bat

---
+++ C:\Users\zhaol\WorkBuddy\20260408101905\OrderSniper\gradlew.bat
@@ -0,0 +1,15 @@
+@rem Gradle wrapper script for Windows
+@if "%DEBUG%" == "" @echo off
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+@rem Setup the command line
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+@rem Execute Gradle
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
