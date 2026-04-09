74995800cccedafdcf6001e3d1e68dd3dff715a1
zhaolianduo <36400601+zhaolianduo@users.noreply.github.com>
2026-04-09 18:46:28 +0800

    Add C:\Users\zhaol\WorkBuddy\20260408101905\OrderSniper\app\src\main\java\com\ordersniper\AppConstants.java

---
+++ C:\Users\zhaol\WorkBuddy\20260408101905\OrderSniper\app\src\main\java\com\ordersniper\AppConstants.java
@@ -0,0 +1,7 @@
+package com.ordersniper;
+
+public class AppConstants {
+    public static final String PREFS_NAME = "order_sniper_prefs";
+    public static final String KEY_KEYWORDS = "keywords";
+    public static final String KEY_FLOAT_WINDOW_SHOWN = "float_window_shown";
+}
